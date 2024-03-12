4600f9a2078b898a2f4a35d52a2fcc13e3e11cc4
suminkim <di05023@gmail.com>
2024-03-12 13:21:47 +0900

    header 영역 작업중 menu css 완료, jquery 하는 중

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  //gnb 관련 코드
+  var gnb = $(".mainmenu");
+  var dim = $(".header-dim");
+  gnb.mouseenter(function () {
+    dim.stop().fadeIn(200);
+  });
+  gnb.mouseleave(function () {
+    dim.stop().fadeOut(200);
+  });
+});
